6585858a5f14d4398d6b5a4c0435099d77fd8659
Dimka <dimakolbin200@gmail.com>
2024-02-22 00:48:50 +0500

    выбор фона + шрифта

--- sait/cod.js
+++ sait/cod.js
@@ -1,6 +1,8 @@
 // создание окна с введённым с блока с id textInput текстом
 function createNewWindow() {
     var inputText = document.getElementById('textInput').value;
+    var bgColor = document.getElementById('bgColor').value;
+    var textColor = document.getElementById('textColor').value;
     var newWindow = window.open('', '', 'width=400,height=200');
-    newWindow.document.write('<p>' + inputText + '</p>');
+    newWindow.document.write('<body style="background-color: ' + bgColor + '; color: ' + textColor + ';"><p>' + inputText + '</p></body>');
   }
